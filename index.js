481c2a851cc9f1072b9b84e5f1f73da0350abf61
EarthBars <54781259+EarthBars@users.noreply.github.com>
2019-10-11 09:46:14 +1300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if ((width >= 1000)){
 
         $("#header").css("background","#15157d");
 
-        $("#header").css("color","#000");
+        $("#header").css("color","#ffffff");
 
         $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
 
